1e5d957197c4d7688047dcbaf7653e57c51eae27
brandwavestudio <brandwavestudio@gmail.com>
2025-02-09 14:49:09 +0530

    Working with DOM Headings and attribute

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 ## Learning README.md file
 
 <h1>Javascript Scripts</h1>
-
-<h3>Demo1.html</h3>
-<p>Printing h1 element content to h2 with getElementsByTagName() method</p>
-
+<br><br>
+<p>Demo1.html</p>
+<h2>Printing h1 element content to h2 with getElementsByTagName() method</h2>
 <hr>
+<p>Demo2.html</p>
+<h2>CRUD h1 element content.</h2>
+<hr>
+<p>Demo3.html</p>
+<h2>Set new attribute & its value to the html elements.</h2>
+<hr>
+
+
+
+
--- javascript/assets/JS/myfile.js
+++ javascript/assets/JS/myfile.js
@@ -10,3 +10,74 @@ function change_dom_element(){
     alert(content)
 }
 
+//demo2.html
+let container = document.getElementById("demo")
+
+function dom_original(){
+    //alert(container.innerHTML)
+    container.innerText = "Change & delete h1 element content."
+}
+
+function dom_change(){
+    container.innerHTML = "I Love My Parents"
+}
+
+function dom_delete(){
+    container.innerHTML = ""
+}
+
+function dom_insert(){
+    let content = document.getElementById("content_data").value;
+    // alert(content)
+    container.innerHTML = content;
+    document.getElementById("content_data").value = ""
+}
+
+
+
+//demo3.html
+function dom_attribute(){
+    let h1_ref = document.getElementsByTagName("h1")[0]
+
+    h1_ref.setAttribute('class','my_class_name')
+    h1_ref.setAttribute('id','my_id_name')
+    h1_ref.setAttribute('title','Welcome to Brand Wave Studio')
+    h1_ref.setAttribute('lang','en')
+    h1_ref.setAttribute('data-info','Special Heading')
+}
+
+
+//demo4.html
+function current_attribute(){
+    let container_ref = document.getElementById("demo")
+
+    attribute = container_ref.attributes
+    // console.log(attribute)
+    for(i=0;i<attribute.length;i++){
+        console.log(attribute[i])
+        alert(attribute[i].name + " = " + attribute[i].value);
+    }
+}
+
+
+//demo5.html
+function change_attribute(apply_class){
+    //alert(apply_class)
+    let container_ref = document.getElementById("demo");
+
+    if(apply_class == "text_primary"){
+        //alert(apply_class)
+        container_ref.setAttribute("class","text_primary")
+    }else if(apply_class == "text_danger"){
+       // alert(apply_class)
+        container_ref.setAttribute("class","text_danger")
+    }else if(apply_class == "text_success"){
+       // alert(apply_class)
+        container_ref.setAttribute("class","text_success")
+    }else{
+        container_ref.setAttribute("class","text_primary")
+    }
+}
+
+
+
